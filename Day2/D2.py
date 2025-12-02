974e9ff4e2107362eaace30d8ec8b102cd0c82cb
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2025-12-02 14:39:09 +0100

    Finish D2 part1

---
+++ Day2/D2.py
@@ -0,0 +1,36 @@
+
+full_input = open("./input").read()
+ranges = full_input.split(",")
+
+
+# takes single ID string and returns true if ID is bad
+def check_id(ident: str) -> bool:
+    if len(ident) % 2 != 0:
+        return False
+
+    first_half = ident[:len(ident)//2]
+    last_half = ident[len(ident)//2:]
+
+    return first_half == last_half
+    
+def main():
+    print("Running main script")
+
+    total = 0
+    for j in range(len(ranges)):
+        start = int(ranges[j].split("-")[0])
+        stop = int(ranges[j].split("-")[1])
+
+        for j in range(start, stop+1):
+            if check_id(str(j)):
+                total += j
+
+    print("total is:")
+    print(total)
+
+
+
+
+
+main()
+
