0bb5c6e79749c2e87bce2d305d26c5c1ee9913c8
Cornelius-Sivertsen <150359082+Cornelius-Sivertsen@users.noreply.github.com>
2025-12-23 14:20:56 +0100

    Finish day5 part1

--- Day5/Day5_Part1.cpp
+++ Day5/Day5_Part1.cpp
@@ -3,150 +3,59 @@
 #include <vector>
 #include <cassert>
 
-// day4 part 1
+// day5 part 1
 
 using namespace std;
 
-enum position_state_t{
-  MIDDLE,
-  TOP_EDGE,
-  BOT_EDGE,
-  LEFT_EDGE,
-  RIGHT_EDGE,
-  TL_CORNER,
-  TR_CORNER,
-  BL_CORNER,
-  BR_CORNER
-};
-
-position_state_t find_state(int row, int column, int max_rows, int max_columns){
-
-  if (row == 0 && column == 0){
-    return TL_CORNER;
-  }
-  else if (row == 0 && column == max_columns-1){
-    return TR_CORNER;
-  }
-  else if (row == 0){
-    return TOP_EDGE;
-  }
-  else if (row == max_rows-1 && column == max_columns-1){
-    return BR_CORNER;
-  }
-  else if (column == max_columns-1){
-    return RIGHT_EDGE;
-  }
-  else if (row == max_rows-1 && column == 0){
-    return BL_CORNER;
-  }
-  else if (row == max_rows-1){
-    return BOT_EDGE;
-  }
-  else if (column == 0){
-    return LEFT_EDGE;
-  }
-  else{
-    return MIDDLE;
-  }
+bool is_a_digit(char input){
+  return input == '0' || input == '1' || input == '2' || input == '3' || input == '4' || input == '5' || input == '6' || input == '7' || input == '8' || input == '9';
 }
 
-int count_around(size_t row, size_t column, vector<string> table){
-  position_state_t state = find_state(row,column,table.size(),table[0].size());
-
-  size_t row_min, row_max, col_min, col_max;
-
-  switch(state){
-  case MIDDLE:
-    row_min = row - 1;
-    row_max = row + 1;
-    col_min = column - 1;
-    col_max = column + 1;
-    break;
-  case TOP_EDGE:
-    row_min = row;
-    row_max = row + 1;
-    col_min = column - 1;
-    col_max = column + 1;
-    break;
-  case BOT_EDGE:
-    row_min = row - 1;
-    row_max = row;
-    col_min = column - 1;
-    col_max = column + 1;
-    break;
-  case LEFT_EDGE:
-    row_min = row - 1;
-    row_max = row + 1;
-    col_min = column;
-    col_max = column + 1;
-    break;
-  case RIGHT_EDGE:
-    row_min = row - 1;
-    row_max = row + 1;
-    col_min = column - 1;
-    col_max = column;
-    break;
-  case TL_CORNER:
-    row_min = row;
-    row_max = row + 1;
-    col_min = column;
-    col_max = column + 1;
-    break;
-  case TR_CORNER:
-    row_min = row;
-    row_max = row + 1;
-    col_min = column - 1;
-    col_max = column;
-    break;
-  case BL_CORNER:
-    row_min = row - 1;
-    row_max = row;
-    col_min = column;
-    col_max = column + 1;
-    break;
-  case BR_CORNER:
-    row_min = row - 1;
-    row_max = row;
-    col_min = column - 1;
-    col_max = column;
-    break;
-  }
-
-  int count = 0;
-
+size_t find_split_index(vector<string> input_data){
   int i;
-  int j;
-
-  for(i = row_min; i<=row_max; i++){
-    for(j = col_min; j<=col_max;j++){
-      if (table[i][j] == '@'){
-	if (!(i == row && j == column)){
-	  count++;
-	}
-      }
+  for (i = 0;i<input_data.size();i++){
+    if (!is_a_digit(input_data[i][0])){
+      return i;
     }
   }
-  return count;
+  return -1;
+}
+
+bool num_in_range(double num, string range){
+  string delim = "-";
+  string lower_bound_s = range.substr(0, range.find(delim));
+  string upper_bound_s = range.substr(range.find(delim)+1);
+
+  double lower_bound = stod(lower_bound_s);
+  double upper_bound = stod(upper_bound_s);
+
+  return (num >= lower_bound && num <= upper_bound);
+}
+
+bool num_in_ranges(double num, vector<string> ranges){
+  for (int i = 0; i<ranges.size(); i++){
+    if (num_in_range(num,ranges[i])){
+      return true;
+    }
+  }
+  return false;
 }
 
 int run_case(vector<string> input_data){
-  int free_rolls = 0;
 
-  int i,j;
-  int tmp;
-  for(i=0;i<input_data.size();i++){
-    for(j=0;j<input_data[0].size();j++){
-      if (input_data[i][j] == '@'){
-	tmp = count_around(i,j,input_data);
-	if (count_around(i,j,input_data) < 4){
-	  free_rolls++;
-	}
-      }
+  size_t split_index = find_split_index(input_data);
+  vector<string> ids(input_data.begin(), input_data.begin() + split_index);
+  vector<string> ingredients(input_data.begin()+split_index+1, input_data.end());
+
+  int fresh_ingredients = 0;
+
+  for (int i = 0; i < ingredients.size(); i++){
+    if (num_in_ranges(stod(ingredients[i]),ids)){
+      fresh_ingredients++;
     }
   }
-  return free_rolls;
+  return fresh_ingredients;
 }
-
 
 
 int main(int argc, char *argv[]){
@@ -174,6 +83,6 @@ int main(int argc, char *argv[]){
   int output = run_case(input);
   cout << "result: " << output << endl << flush;
   if (testing){
-    assert(output == 13);
+    assert(output == 3);
   }
 }
